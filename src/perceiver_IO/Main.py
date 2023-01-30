b6428d4c19350d2f5dfe00684500df6846560d58
Saurabh <saurabhdeshpande93@gmail.com>
2023-01-30 20:15:52 +0100

    added comments

--- src/perceiver_IO/Main.py
+++ src/perceiver_IO/Main.py
@@ -18,18 +18,16 @@ if case == "3D":
 	name = "3D"
 	weights_path = srcpath+"/frontiers_data/saved_models/3dperceiver.pt"
 	prediction_path = srcpath+"/frontiers_data/predictions/3dperceiver_predicts.npy"
-	in_size=5835
+	in_size=5835  #Degrees of freedom of the 3D problem
 elif case == "2D":
 	datapath = srcpath + "/frontiers_data/FEMData/"
 	name = "2D"
 	weights_path = srcpath+"/frontiers_data/saved_models/2dperceiver.pt"
 	prediction_path = srcpath+"/frontiers_data/predictions/2dperceiver_predicts.npy"
-	in_size=512
+	in_size=512  #Degrees of freedom of the 2D problem
 
 
 perceiver = Model.SCCPerceiver(in_size)
 perceiver.LoadDataset(datapath,name,trainable=True)
 perceiver.Load(weights_path)
 perceiver.SaveTestInference(prediction_path)
-
-
